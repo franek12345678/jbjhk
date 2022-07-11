28c4b2b76bd39210cfa460142a394a59bf845bae
franek12345678 <109067977+franek12345678@users.noreply.github.com>
2022-07-11 13:47:04 +0200

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ led.setBrightness(255)
 loops.everyInterval(60000, function () {
     basic.showNumber(input.runningTime())
     basic.showLeds(`
-        # # # # .
+        # # # # #
         # # # # #
         # # # # #
         # # # # #
