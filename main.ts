a2d9252e4df7dde096f1e03a992c7f27f1db5822
franek12345678 <109067977+franek12345678@users.noreply.github.com>
2022-07-11 13:15:37 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+loops.everyInterval(60000, function () {
+    basic.showNumber(input.runningTime())
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+	
+})
